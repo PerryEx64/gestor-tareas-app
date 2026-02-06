200cc1b05d9bf5956d6b9bbd3c898cacba3bcd06
perryex64 <brianisrael74@gmail.com>
2026-02-06 12:38:57 -0600

    Remove unused styles from ConfigurationAuthSheet component

--- screens/public/auth/components/ConfigurationAuthSheet.tsx
+++ screens/public/auth/components/ConfigurationAuthSheet.tsx
@@ -30,18 +30,3 @@ export const ConfigurationAuthSheet = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  card: {
-    borderRadius: 24,
-    marginBottom: 10,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 4,
-    },
-    shadowOpacity: 0.1,
-    shadowRadius: 12,
-    elevation: 5,
-  },
-});
